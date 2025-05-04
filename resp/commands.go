44d6e007a04494fc5fa7dc23be58e66b676bb712
pratham-exe <prathamrao001@gmail.com>
2025-05-04 17:00:15 +0530

    HSET HGET command

--- resp/commands.go
+++ resp/commands.go
@@ -6,6 +6,8 @@ var Command_store = map[string]func([]Client_input) Client_input{
 	"PING": command_ping,
 	"SET":  command_set,
 	"GET":  command_get,
+	"HSET": command_hset,
+	"HGET": command_hget,
 }
 
 func command_ping(args []Client_input) Client_input {
@@ -53,4 +55,50 @@ func command_get(args []Client_input) Client_input {
 	}
 
 	return Client_input{Tipe: "bulk", Bulk: get_comm_value}
+}
+
+var (
+	hset_command_hashmap = map[string]map[string]string{}
+	hset_command_mutex   = sync.RWMutex{}
+)
+
+func command_hset(args []Client_input) Client_input {
+	if len(args) != 3 {
+		return Client_input{Tipe: "error", Str: "I only take 3 arguments."}
+	}
+
+	hset_comm_hash := args[0].Bulk
+	hset_comm_key := args[1].Bulk
+	hset_comm_value := args[2].Bulk
+
+	hset_command_mutex.Lock()
+
+	_, ok := hset_command_hashmap[hset_comm_hash]
+	if !ok {
+		hset_command_hashmap[hset_comm_hash] = map[string]string{}
+	}
+	hset_command_hashmap[hset_comm_hash][hset_comm_key] = hset_comm_value
+
+	hset_command_mutex.Unlock()
+
+	return Client_input{Tipe: "string", Str: "OK"}
+}
+
+func command_hget(args []Client_input) Client_input {
+	if len(args) != 2 {
+		return Client_input{Tipe: "error", Str: "I only take 2 arguments."}
+	}
+
+	hget_comm_hash := args[0].Bulk
+	hget_comm_key := args[1].Bulk
+
+	hset_command_mutex.RLock()
+	hget_comm_value, ok := hset_command_hashmap[hget_comm_hash][hget_comm_key]
+	hset_command_mutex.RUnlock()
+
+	if !ok {
+		return Client_input{Tipe: "nill"}
+	}
+
+	return Client_input{Tipe: "bulk", Bulk: hget_comm_value}
 }
